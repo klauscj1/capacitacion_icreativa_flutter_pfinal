487078675cf899bd8299f4956f02b331063819c3
Claus Chocho <clauschochoe@gmail.com>
2020-11-06 09:55:27 -0500

    detail ready

--- lib/ui/pages/detail_product_page.dart
+++ lib/ui/pages/detail_product_page.dart
@@ -33,6 +33,9 @@ class DetailProductPage extends StatelessWidget {
             height: MediaQuery.of(context).size.height * .05,
           ),
           secundariosTextos(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .01,
+          ),
           appbarPedido(context)
         ],
       ),
@@ -40,181 +43,150 @@ class DetailProductPage extends StatelessWidget {
   }
 
   Widget secundariosTextos(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: size.height * .24,
-        color: Theme.of(context).accentColor,
-        child: Row(
-          children: [
-            Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  producto.descripcion,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      producto.nombre,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      maxLines: 2,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.height * .07,
-                    ),
-                    crearbotonesPedido(context, Icons.add, false),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.height * .001,
-                    ),
-                    crearbotonesPedido(context, Icons.open_with, false),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.height * .001,
-                    ),
-                    crearbotonesPedido(context, Icons.remove, false)
-                  ],
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .04,
-                ),
-                Text(
-                  producto.descripcion+'  '+producto.nombre,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    producto.descripcion,
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                ),
-                Text(
-                  producto.descripcion+'  '+producto.nombre,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                Text(
-                  producto.nombre,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .04,
-                ),
-
-                 Row(
-                  children: [
-                    Text('Delivery Time',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                    SizedBox(
-                      width: 10,
+                  Text(
+                    producto.nombre,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Icon(Icons.lock_clock),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text('25 Mins',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+                    maxLines: 2,
                   ),
-                ),
-                  ],
-                ),
-                
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.height * .07,
+                  ),
+                  crearbotonesPedido(context, Icons.add, false),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.height * .001,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      '1',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.height * .001,
+                  ),
+                  crearbotonesPedido(context, Icons.remove, false)
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .01,
+          ),
+          Text(producto.receta),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .01,
+          ),
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Delivery Time',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.lock),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '25 Mins',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   Widget appbarPedido(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: size.height * .14,
-        color: Theme.of(context).accentColor,
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                 SizedBox(
-                      height: MediaQuery.of(context).size.height * .01,
-                    ),
-                 Text('Total Price',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w300,
-                  ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Total Price',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
                 ),
-                SizedBox(
-                      height: MediaQuery.of(context).size.height * .01,
-                    ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                          '\$${producto.precio}',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxLines: 2,
-                        ),
-                        SizedBox(
-                      width: MediaQuery.of(context).size.height * .23,
-                    ),
-
-                        Container(
-                      width: 50,
-                      height: 50,
-                       decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        topLeft: Radius.circular(10),
-                      ),
-                    ),
-                      child: Center(
-                        child: Icon(
-                          Icons.shopping_cart,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        ),
-                        )
-                  ],
-
-                  
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              Text(
+                '\$${producto.precio}',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
-               
-                
-                        
-              ],
-              
-            )
-          
-          ],
-        ),
-
-        
+                maxLines: 2,
+              ),
+            ],
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                topLeft: Radius.circular(10),
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
